f208d45db5ca36e3f2022faa4fc9c8f129ed6048
Anmol Rana <aanmol.rrana@gmail.com>
2021-07-24 16:50:31 +0530

    Second Commit

--- backend/data.js
+++ backend/data.js
@@ -8,7 +8,8 @@ export default {
         price: 60,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock: 0
       },
       {
         _id: '2',
@@ -18,7 +19,8 @@ export default {
         price: 50,
         brand: ' Nike',
         rating: 4.2,
-        numReviews: 5
+        numReviews: 5,
+        countInStock: 9
       },
       {
         _id: '3',
@@ -28,7 +30,8 @@ export default {
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 16
       }, {
         _id: '4',
         name: 'Best Pants',
@@ -37,7 +40,30 @@ export default {
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 3
+      },
+      {
+        _id: '5',
+        name: 'Jeans',
+        category: 'pants',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5,
+        countInStock: 9
+      },
+      {
+        _id: '6',
+        name: 'Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 50,
+        brand: ' Nike',
+        rating: 4.2,
+        numReviews: 5,
+        countInStock: 9
       },
     ]
   }
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import data from './data';
 const app =express();
 
-app.get("/api/products/:id",(req,res)=>{
+app.get("/api/products/:id", (req,res)=>{
     const productId = req.params.id;
     const product =  data.products.find(x=>x._id === productId);
    if (product)
