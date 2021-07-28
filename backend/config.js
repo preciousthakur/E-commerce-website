58327cf15f19edda7a8554a27e784f7cc27a246a
Anmol Rana <aanmol.rrana@gmail.com>
2021-07-28 11:21:19 +0530

    Authentication

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
-MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona'
+export default{
+MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/e-commerce',
+JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+};
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,22 @@ const app =express();
 import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
-
+import userRoute from './routes/userRoute';
+import bodyParser from 'body-parser';
 dotenv.config();
-const mongodburl = config.MONGODB_URL;
-mongoose.connect(mongodburl,{
-    useNewUrlParser: true
+
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 }).catch(error => console.log(error.reason));
 
+
+
+app.use(bodyParser.json());
+app.use("/api/users", userRoute);
 
 app.get("/api/products/:id", (req,res)=>{
     const productId = req.params.id;
